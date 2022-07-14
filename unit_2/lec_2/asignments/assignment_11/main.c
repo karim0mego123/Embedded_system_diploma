89c33ff3f1996cc8a408aca924c63a1fdb9cbedd
karim EL_mlegy <karim.ayman.kemo@gmail.com>
2022-07-14 03:15:33 +0200

    add new condation

--- unit_2/lec_2/asignments/assignment_11/main.c
+++ unit_2/lec_2/asignments/assignment_11/main.c
@@ -12,7 +12,8 @@ int main ()
 	printf("enter any number = ") ;
 	fflush(stdin) ; fflush(stdout) ;
 	scanf("%d",&x) ;
-	(x>0)? printf("number is positave") : printf("number is nagitave") ;
+	(x>0)? printf("number is positave") : (x==0)? printf("num is zero") :
+			printf("number is nagitave") ;
 	return 0 ;
 }
 
