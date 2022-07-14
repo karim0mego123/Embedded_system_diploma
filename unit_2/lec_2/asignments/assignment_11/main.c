c441e31f4d88e66b5079684cd296d501c0bb4d24
karim EL_mlegy <karim.ayman.kemo@gmail.com>
2022-07-14 03:12:12 +0200

    assignment_11

---
+++ unit_2/lec_2/asignments/assignment_11/main.c
@@ -0,0 +1,18 @@
+/*
+ * main.c
+ *
+ *  Created on: Jul 14, 2022
+ *      Author: karim
+ */
+#include "stdio.h"
+
+int main ()
+{
+	int x ;
+	printf("enter any number = ") ;
+	fflush(stdin) ; fflush(stdout) ;
+	scanf("%d",&x) ;
+	(x>0)? printf("number is positave") : printf("number is nagitave") ;
+	return 0 ;
+}
+
